f0b6478aba6ef8ac658d47d2adb8a474d33538cc
mrblondin <dm.usmanov@gmail.com>
2024-02-09 15:56:02 +0100

    Updated Python solution Rotational Cipher to understand maketrans() and translate() functions

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,22 +1,35 @@
-ALPA_NUM = 26
-
-
-def rotate_char(code, code_a, key):
-    return chr((code - code_a + key) % ALPA_NUM + code_a)
-
-
 def rotate(text, key):
-    result = ''
-    char_a = ord('a')
-    char_A = ord('A')
-    for char in [*text]:
-        if char.isalpha():
-            ascii_code = ord(char)
-            if char.isupper():
-                result += rotate_char(ascii_code, char_A, key)
-            else:
-                result += rotate_char(ascii_code, char_a, key)
-        else:
-            result += char
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-    return result
+    translator = ALPHABET[key:] + ALPHABET[:key]
+
+    transtab = str.maketrans(ALPHABET + ALPHABET.upper(), translator +
+                             translator.upper())
+
+    return text.translate(transtab)
+
+# below is my initial solution.
+# decided to rewrite my solution to try the translation function
+#
+# ALPA_NUM = 26
+#
+#
+# def rotate_char(code, code_a, key):
+#     return chr((code - code_a + key) % ALPA_NUM + code_a)
+#
+#
+# def rotate(text, key):
+#     result = ''
+#     char_a = ord('a')
+#     char_A = ord('A')
+#     for char in [*text]:
+#         if char.isalpha():
+#             ascii_code = ord(char)
+#             if char.isupper():
+#                 result += rotate_char(ascii_code, char_A, key)
+#             else:
+#                 result += rotate_char(ascii_code, char_a, key)
+#         else:
+#             result += char
+#
+#     return result
