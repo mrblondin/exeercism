8a7378d6412f8782b55313104234e6e0e6161bc3
mrblondin <dm.usmanov@gmail.com>
2024-02-15 21:18:06 +0100

    Added JS solution Nucleotide Count

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,8 +1,11 @@
-//
-// This is only a SKELETON file for the 'Nucleotide Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const NUCLEOS = 'ACGT';
 
 export function countNucleotides(strand) {
-  throw new Error('Remove this statement and implement this function');
+  if (/[^ACGT]/.test(strand)) throw new Error('Invalid nucleotide in strand');
+  const map = [...strand].reduce((acc, cur) => {
+    acc[cur] = (cur in acc) ? acc[cur] + 1 : 1;
+    return acc;
+  }, {});
+
+  return [...NUCLEOS].reduce((acc, cur) => { acc.push(cur in map ? map[cur] : 0); return acc }, []).join(' ');
 }
