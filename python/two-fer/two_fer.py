bb7b8e184acd84106bc4786e9e2a7aaa829b79aa
mrblondin <dm.usmanov@gmail.com>
2024-02-24 20:25:37 +0100

    Added Python solution Two Fer

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,7 @@
+def two_fer(name="you"):
+    """ Two fer function
+
+    :param name: str - the name of the opponent
+    :return: str - the appropriate answer
+    """
+    return f"One for {name}, one for me."
