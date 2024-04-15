88584177dd9fa02460743a3503e4fb394bbc97a9
mrblondin <dm.usmanov@gmail.com>
2024-04-15 22:17:34 +0200

    Fixed C solution Saddle Points

--- c/saddle-points/saddle_points.c
+++ c/saddle-points/saddle_points.c
@@ -3,55 +3,47 @@
 #define ERROR 0
 
 saddle_points_t * saddle_points(size_t rows, size_t columns, uint8_t matrix[rows][columns]){
-
-  // allocate maximum possible memory according to matrix dimensions
-  saddle_points_t * points = malloc(
+    // allocate maximum possible memory according to matrix dimensions
+    saddle_points_t * points = malloc(
     sizeof(*points) + rows*columns*sizeof(saddle_point_t));
 
-  if (!points) return ERROR;
+    if (!points) return ERROR;
 
-  points->count = 0;
+    points->count = 0;
 
-  for (size_t i = 0; i < rows; i++){
+    uint8_t max_row_values[rows];
+    memset(max_row_values, 0, sizeof(max_row_values));
 
-    uint8_t maxInRow = 0;
-    uint8_t countInRow = 0;
+    // calculate max values in rows
+    for (size_t row = 0; row < rows; ++row){
+        for (size_t col = 0; col < columns; ++col){
+            max_row_values[row] = matrix[row][col] > max_row_values[row] ?
+                matrix[row][col] : max_row_values[row];
+        } 
+    }
 
-    for(size_t j = 0; j < columns; j++){
-      if (matrix[i][j] >= maxInRow) {
-        // this is the candidate
 
-        // if new candidate in a row, we need refresh counters
-        if (matrix[i][j] > maxInRow) {
-          maxInRow = matrix[i][j];
-          points->count -= countInRow;
-          countInRow = 0;
+    // find min values in columns and compare it with max_row_values
+    for (size_t col = 0; col < columns; ++col){
+        uint8_t min_col_value = -1;
+
+        for (size_t row = 0; row < rows; ++row){
+            uint8_t cur_val = matrix[row][col];
+            if (cur_val > min_col_value) continue;
+            min_col_value = cur_val;
         }
 
-        // check if the candidate matches the condition in columns
-        int success = 1;
-        for(size_t k = 0; k < rows; k++){
-          if (matrix[i][j] > matrix[k][j]){
-            success = 0;
-            break;
-          }
+        for (size_t row = 0; row < rows; ++row){
+            uint8_t cur_val = matrix[row][col];
+            if (cur_val == min_col_value && cur_val == max_row_values[row])
+                points->points[points->count++] = (saddle_point_t){row + 1, col + 1};
         }
+    }
 
-        if (success){
-          points->points[points->count++] = (saddle_point_t){i+1,j+1};
-          countInRow = maxInRow == matrix[i][j] ?  countInRow + 1  : 1;
-        }
-      } 
-    } 
-  }
-
-  points = realloc(points, sizeof(*points) + points->count*sizeof(saddle_point_t));
-  return points;
+    points = realloc(points, sizeof(*points) + points->count*sizeof(saddle_point_t));
+    return points;
 }
 
 void free_saddle_points(saddle_points_t * points) {
-  if (!points) return;
-
-  free(points);
+    free(points);
 }
-
--- c/saddle-points/saddle_points.h
+++ c/saddle-points/saddle_points.h
@@ -1,6 +1,7 @@
 #ifndef SADDLE_POINTS_H
 #define SADDLE_POINTS_H
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
 typedef struct {
@@ -14,18 +15,19 @@ typedef struct {
 } saddle_points_t;
 
 /**
- * Find the appropriate coordinates, minimum in a row and max in a column
- * @param (size_t) rows, number of rows in matrix
- * @param (size_t) columns, number of columns in matrix
- * @param (uint8_t[]) matrix, array filled with numbers
- * returns (saddle_points_t *) suitable coordinates
+ * Find the appropriate coordinates, minimum in a row and max in a column.
+ *
+ * @param rows, number of rows in matrix
+ * @param columns, number of columns in matrix
+ * @param matrix, array filled with numbers
+ * @returns suitable coordinates
  */
 saddle_points_t * saddle_points(size_t rows, size_t columns, uint8_t matrix[rows][columns]);
 
 /**
- * Release saddle points value from the memory
- * @param (saddle_points_t) points to free
- * returns (void)
+ * Release saddle points value from the memory.
+ *
+ * @param points to free
  */
 void free_saddle_points(saddle_points_t * points);
 
